e2a762c4564ac17a4dc6b15092b662c5d154e580
roshni-chakor <101988703+roshni-chakor@users.noreply.github.com>
2022-03-20 10:09:35 -0700

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,23 @@
+num1=int(input("enter number1:"))
+num2=int(input("enter number2:"))
+result_1=num1+num2
+result_2=num1-num2
+result_3=num1*num2
+result_4=num1/num2
+print(num1,"+",num2,"=",result_1)
+print(num1,"-",num2,"=",result_2)
+print(num1,"*",num2,"=",result_3)
+print(num1,"/",num2,"=",result_4)
+
+
+
+
+
+
+
+
+
+num1=int(input("enter number1:"))
+num2=int(input("enter number2:"))
+result_1=(num1+num2)/2
+print(num1,"+",num2,"/"2,"=",result_1)
